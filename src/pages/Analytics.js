ce26749a70b60e0dfed07dc0912d8597002580b5
Ananth <106952901+Ananthm032@users.noreply.github.com>
2022-12-26 18:03:18 +0530

    first commit

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import LineChart from '../components/Chart/LineChart/LineChart'
-import Pie from '../components/Chart/Pipechart/PipeChart'
+import React from "react";
+import { Chart } from "react-google-charts";
 import "../Styles/Analytics.css"
-function Analytics() {
-  return (
-    <div className='tic'>
-      <LineChart/>
-       <Pie/>
-    </div>
-  )
-}
 
-export default Analytics
+export const data = [
+  ["Country",""],
+  ["Germany", 200],
+  ["United States", 300],
+  ["Brazil", 400],
+  ["Canada", 500],
+  ["France", 600],
+  ["RU", 700],
+  ["INDIA", 1000]
+];
+
+export default function Analaytic() {
+  return (
+    <Chart className="Chart"
+      chartEvents={[
+        {
+          eventName: "select",
+          callback: ({ chartWrapper }) => {
+            const chart = chartWrapper.getChart();
+            const selection = chart.getSelection();
+            if (selection.length === 0) return;
+            const region = data[selection[0].row + 1];
+            console.log("Selected : " + region);
+          },
+        },
+      ]}
+      chartType="GeoChart"
+    
+      data={data}
+    />
+  );
+}
--- src/pages/CelebrityNFT.js
+++ src/pages/CelebrityNFT.js
@@ -1,56 +1,8 @@
-import React,{useState}from 'react'
-import "../Styles/CelebrityNFT.css"
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import Img from "../components/Images/v795-tang-42b.png"
+import React from 'react'
+
 function CelebrityNFT() {
-
-  const[image,setImage] =useState(null)
-  const[fileName,setFileName] =useState("No Selected FIle")
-
-  const[image1,setImage1] =useState(null)
-  const[fileName1,setFileName1] =useState( "" )
-
-
-
-  
   return (
-    <div className='infoPage'>
-      <div className='info-top'>
-  <form className='form'  action=""
-  onClick={ ()=> document.querySelector(".input-field").click()}>
-    <input type="file" accept='image/*' className='input-field' hidden style={{width:"200px" , height:"200px",border:"2px solid red"}}
-
-    onChange={({target :{files}})=>{
-      files[0] && setFileName(files[0].name)
-      if(files){
-        setImage(URL.createObjectURL(files[0]))
-      }
-    }}
-/>
-  {image ? <img src={image} className="img" alt={fileName}/> : <CloudUploadIcon sx={{scale:"2.3"}}/>}
-
-  </form>
-  </div>
-  <div className='info-mid'>
-<h1>
-<form  className='form-1' action=""
-  onClick={ ()=> document.querySelector(".input-field1").click()}>
-    <input type="file" accept='image/*'  className='input-field1' hidden
-    
-    onChange={({target :{files}})=>{
-      files[0] && setFileName1(files[0].name)
-      if(files){
-        setImage1(URL.createObjectURL(files[0]))
-      }
-    }}
-/>
-  {image ? <img src={image1}  className='img-1'  alt={fileName1}/> : 
-""
-    } 
-  </form>
-</h1>
-  </div>
-    </div>
+    <div>CelebrityNFT</div>
   )
 }
 
